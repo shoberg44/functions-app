78980d6ec0697bf164084c2ae1462aa7242ea9f3
shoberg44 <steven.hoberg23@d155.org>
2022-09-23 08:05:44 -0500

    init commit

--- functions app/ViewController.swift
+++ functions app/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gradeIn: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func gradeButtonAction(_ sender: UIButton) {
+        pass(Double(gradeIn.text ?? "0.00"))
+    }
+    
+    func pass(_ num: Double)->String{
+        
+        if num < 50{
+            return "fail"
+        }
+        else{
+            return "pass"
+        }
+        
+    }
+    
 
 }
 
