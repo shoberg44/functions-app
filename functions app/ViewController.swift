04a2fda23a28a6cdff4ca9d600bc990a6e5b27cb
shoberg44 <steven.hoberg23@d155.org>
2022-09-26 08:52:45 -0500

    done but bug

--- functions app/ViewController.swift
+++ functions app/ViewController.swift
@@ -9,24 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passOutput: UILabel!
     @IBOutlet weak var gradeIn: UITextField!
+    @IBOutlet weak var points1In: UITextField!
+    @IBOutlet weak var points2In: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     @IBAction func gradeButtonAction(_ sender: UIButton) {
-        pass(Double(gradeIn.text ?? "0.00"))
+        print("->\(gradeIn.text)")
+        if gradeIn.text! != "" {
+        passOutput.text = "\(pass(Double(gradeIn.text!) ?? 0.00)) \(letter(Double(gradeIn.text!) ?? 0.00))"
+        }
+        else{
+            let temp = points(Double(points1In.text!) ?? 0.00,Double(points2In.text!) ?? 0.00)
+            passOutput.text = "\(temp.0) \(temp.1)"
+        }
+        
     }
     
     func pass(_ num: Double)->String{
         
         if num < 50{
-            return "fail"
+            return "Fail"
         }
         else{
-            return "pass"
+            return "Pass"
         }
         
+    }
+    func letter(_ num: Double)->String{
+        if num >= 90{
+            return "A"
+        }
+        else if num >= 80{
+            return "B"
+        }
+        else if num >= 70{
+            return "C"
+        }
+        else if num >= 60{
+            return "D"
+        }
+        else{
+            return "F"
+        }
+    }
+    func points(_ num1: Double, _ num2: Double)->(Double,String){
+        let numOut = num1 / num2
+        let letter = letter(numOut)
+        return (numOut,letter)
     }
     
 
